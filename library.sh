54f75fe23fff4456c01d594ced9933b53349984f
Wai Yan Wan <waiyanwan@Waide-MacBook-Pro.local>
2020-06-07 12:34:31 +0800

    update library script

--- library.sh
+++ library.sh
@@ -1,1 +1,9 @@
+if which node > /dev/null
+    then
+        echo "node is installed, skipping..."
+    else
+        curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+        sudo apt install nodejs
+        node --version
+    fi
 npm install
