3b5db8cc2a4aa00e7e3497070ff8265e32bea434
Wai Yan Wan <waiyanwan@Waide-MacBook-Pro.local>
2020-06-07 23:42:24 +0800

    add hello world page

--- main.js
+++ main.js
@@ -16,7 +16,14 @@ connection.query('SELECT * from < table name >', function(err, rows, fields) {
 });
 
 connection.end(); */
-
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 function sum(a,b) {
   return a+b+1;
