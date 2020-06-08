9eb8d0a01f1d87155a037058d76f7a5ddfe62e13
Wai Yan Wan <waiyanwan@Waide-MacBook-Pro.local>
2020-06-08 21:29:03 +0800

    mysql function

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-/* var mysql      = require('mysql');
+var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : '< MySQL username >',
-  password : '< MySQL password >',
-  database : '<your database name>'
+  host     : 'bdqjmttq7nbu1d6l1kbh-mysql.services.clever-cloud.com',
+  user     : 'u1czxsffks8lxvhc',
+  password : 'ly2PGM7jus3aghfXqtq9',
+  database : 'bdqjmttq7nbu1d6l1kbh'
 });
 
 connection.connect();
 
-connection.query('SELECT * from < table name >', function(err, rows, fields) {
+connection.query('SELECT * from Files', function(err, rows, fields) {
   if (!err)
     console.log('The solution is: ', rows);
   else
     console.log('Error while performing Query.');
 });
 
-connection.end(); */
+connection.end(); 
 
 
 function sum(a,b) {
