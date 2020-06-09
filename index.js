31bdfd725baf744e693e8facee43ac53a9ad63b5
viennew <Vienne.Wan@derivco.com.hk>
2020-06-09 15:11:22 +0800

    fix some bug

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
-const numberOfFiles = require('./main');
+var numberOfFiles = require('./main');
 
 app.get('/', function (req, res) {
-    var num = numberOfFiles();
-    var msg = 'Hello World ! Totally '+ num+ ' files now!';
-    res.send(msg);
+    numberOfFiles().then(function(rows) {
+		var msg = 'Hello World ! Totally '+ rows + ' files now!';
+		res.send(msg);
+	});
+
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
--- main.js
+++ main.js
@@ -1,22 +1,25 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+host     : 'bdqjmttq7nbu1d6l1kbh-mysql.services.clever-cloud.com',
+user     : 'u1czxsffks8lxvhc',
+password : 'ly2PGM7jus3aghfXqtq9',
+database : 'bdqjmttq7nbu1d6l1kbh'
+});
+
 function numberOfFiles() {
-  var numOfFiles = 0;
-  var mysql      = require('mysql');
-  var connection = mysql.createConnection({
-    host     : 'bdqjmttq7nbu1d6l1kbh-mysql.services.clever-cloud.com',
-    user     : 'u1czxsffks8lxvhc',
-    password : 'ly2PGM7jus3aghfXqtq9',
-    database : 'bdqjmttq7nbu1d6l1kbh'
-  });
+	
   connection.connect();
-  connection.query('SELECT * from Files', (err, rows) => {
-    if (!err){
-      numOfFiles = rows.length;
-    }
-    else
-      console.log('Error while performing Query.');
+  
+  return new Promise(function(resolve, reject) {
+	  connection.query('select * from Files', (err, rows) => {
+		  if(err) {
+			return reject(err);
+		  }
+		  resolve(rows.length);
+	  });
+	  connection.end();
   });
-  connection.end(); 
-  return numOfFiles;
+  
 }
 
 module.exports = numberOfFiles;
